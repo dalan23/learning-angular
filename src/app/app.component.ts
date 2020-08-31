7af5027c4f547ada0dfd619b87c2f20c4236fb57
Dalan Official <officialdalan.programming@gmail.com>
2020-08-31 05:21:41 -0400

    feat: Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// this is called a decorator: function applied to a class to modify behavior
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //tag in index to be used
+  templateUrl: './app.component.html', // view of the component
+  styleUrls: ['./app.component.css'] // property to indice stylesheet
 })
 export class AppComponent {
-  title = 'LearningAngular';
+  title = 'Learning Angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyComponent } from './components/my-component/my-component.component';
+import { MoviesComponent } from './components/movies/movies.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, // this declaration helps in the sense of one-import only needed
+    MyComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // Main component that is going to be loaded
 })
 export class AppModule { }
